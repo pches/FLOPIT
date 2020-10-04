de5064a2ac9507d826d14121839b42b78e8ff2ac
mahkam <mxz414@psu.edu>
2020-10-04 12:58:28 -0700

    Rename variables for better readability

--- R_code/FLOPIT_function.R
+++ R_code/FLOPIT_function.R
@@ -27,90 +27,98 @@ require('scales')
 require('gstat')
 
 ################################### FLOPIT function ##########################################
-FLOPIT <- function(flood_rasters, flood_probabilities, elevation_raster, depth, aggregation_value, method, map_type,
+FLOPIT <- function(flood_rasters_names, flood_probabilities, elevation_raster, depth, aggregation_value, method, map_type,
                     save, save_path_data, save_path_map, save_path_zones){
 ################ Import Houston Clipped and Resampled Raster files ###########################
 # be sure to change file paths
-len <- length(flood_rasters)
+n_rp <- length(flood_rasters_names)
+flood_wse_vector <- vector(length = n_rp)
+# calculate return period values for each flood
+return_periods <- 1/flood_probabilities # 4% flood and mean wse were removed due to data quality
+probs_pct <- 100 * (1/return_periods) # probabilities associated with the return periods
 
-flood_raster <- vector(length = len)
-for(i in 1:len){
-str_name <- flood_rasters[i]
-flood_raster[i] <- list(raster(str_name))
-names(flood_raster[[i]])<-'rasterdata'
+# read flood rasters 
+for(i in 1:n_rp){
+    flood_wse_vector[i] <- list(raster(flood_rasters_names[i]))
+    names(flood_wse_vector[[i]])<-'rasterdata'
 }
 
 # import Lidar DEM of the study area
-str_name <- elevation_raster
-elev <- raster(str_name)
-
+dem <- raster(elevation_raster)
+print('Done reading flood rasters and elevation data')
 
 ######################### Convert flood depths (ft) to elevations (ft)
-if(depth==TRUE)(
-# convert depths to elevations
-  for (i in 1:len) {
-    rast_vals <- getValues(flood_raster[[i]])+getValues(elev)
-    flood_raster[i] <- list(setValues(flood_raster[[i]], rast_vals))
-  }
-)
+if(depth==TRUE){
+    # convert depths to water surface elevations
+    for (i in 1:n_rp) {
+        tmp_rast_vals <- getValues(flood_wse_vector[[i]])+getValues(dem)
+        flood_wse_vector[i] <- list(setValues(flood_wse_vector[[i]], tmp_rast_vals))
+    }
+}
+print('Done with calculating water surface elevation')
+
 ######################### Interpolate missing values
-aggval <- aggregation_value # define aggregation size
 # aggregate the rasters
-flood_raster_agg <- vector(length = len)
-for(i in 1:len){
-  flood_raster_agg[i] <- list(aggregate(flood_raster[[i]], fact = aggval, fun = mean))
+returnLevels_wse_upscaled <- vector(length = n_rp)
+
+for(i in 1:n_rp){
+  returnLevels_wse_upscaled[i] <- list(aggregate(flood_wse_vector[[i]], fact = aggregation_value, fun = mean))
 }
 
-elev_agg<-aggregate(elev, fact = aggval, fun = mean)
+dem_upscaled <- aggregate(dem, 
+                      fact = aggregation_value, 
+                      fun = mean)
+print('Done with aggregating elevation and water surface elevation data')
 
-######################### Use Inverse Weighted Distance Interpolation/extrapolation
+######################### Smooth water surface elevation data using Inverse Weighted Distance method
 # define inverse weighted distance interpolation/extrapolation function
 # define raster grid that covers entire interpolation/extrapolation space (DEM suggested for this use)
-rast_grid <- rasterToPoints(elev_agg, spatial = TRUE)
-rast.grid <- rast_grid
-gridded(rast.grid)=TRUE
+flood_raster_grid <- rasterToPoints(dem_upscaled, spatial = TRUE)
+gridded(flood_raster_grid) = TRUE
+
 # define function
-IWD_interp <- function(rast,rast.grid){
-  rast_points <- rasterToPoints(rast, spatial = TRUE)
-  rast.gstat <- gstat(formula = rasterdata ~ 1, data = rast_points, 
-                      nmax = 10, set = list(idp = 0.5))
-  z <- predict(rast.gstat, rast.grid)
-  #  z_rast <- rasterize(z, rast, field = z@data[,1])
-  rast@data@values<-z@data[,1]
-  return(rast)
-}
+IWD_interp <- function(raster,grid){
+    raster_points <- rasterToPoints(raster, spatial = TRUE)
+    raster_gstat <- gstat(formula = rasterdata ~ 1, 
+                          data = raster_points, 
+                          nmax = 10, # for local kriging: the number of nearest observations that should be used for a kriging prediction or simulation
+                          set = list(idp = 0.5)
+                          )
+    z <- predict(rast_gstat, grid)
+    raster@data@values <- z@data[,1]
+    return(raster)
+  }
 
 # start spatial WSE interpolation/extrapolation
-flood_raster_interp <- vector(length = len)
-for (i in 1:len) {
-  flood_raster_interp[i] <- list(IWD_interp(flood_raster_agg[[i]], rast.grid))
-}
+returnLevels_wse_smooth <- vector(length = n_rp)
+for (i in 1:n_rp){
+  returnLevels_wse_smooth[i] <- list(IWD_interp(raster = returnLevels_wse_upscaled[[i]], 
+                                                grid = flood_raster_grid)
+                                     )}
 
 # Test for NA values after interpolation: If there are any, interpolation has made a mistake
-for (i in 1:len) {
-  out <- which(is.na(flood_raster_interp[[i]]@data@values)==TRUE)
+for (i in 1:n_rp) {
+  out <- which(is.na(returnLevels_wse_smooth[[i]]@data@values)==TRUE)
   if(length(out)>0)(print('ERROR: Water surface elevation interpolation failed'))
 }
+print('Done with smooting water surface elevation')
 
 ######################### Interpolate flood probability map
-# calculate return period values for each flood
-returns <- 1/flood_probabilities # 4% flood and mean wse were removed due to data quality
-probs <- 1/returns*100 # probabilities associated with the return periods
 # use pre-existing raster to create new raster of same cell size, extent, projection, etc.
-rt_map <- elev_agg
+rt_map <- dem_upscaled
 # set all values of the new raster to be NA
 setValues(rt_map, NA)
 # define a vector vals to write interpolated flood return periods to
 vals <- vector(mode = 'numeric', length = length(rt_map@data@values))
 # for loop uses splines to define flood elevation to return period for each cell
 # and interpolate the return period associated with each cell's elevation
-elevations <- getValues(elev_agg)
+elevations <- getValues(dem_upscaled)
 pb <- txtProgressBar(min = 0, max = length(getValues(rt_map)), initial = 0, char = '=', style = 1)
 start <- Sys.time()
-floods <- vector(length = len)
+floods <- vector(length = n_rp)
 for (i in 1:length(rt_map@data@values)) {
   setTxtProgressBar(pb, i)
-  for (j in 1:len) {floods[j] <- flood_raster_interp[[j]]@data@values[i]}
+  for (j in 1:n_rp) {floods[j] <- returnLevels_wse_smooth[[j]]@data@values[i]}
   floods <- sort(floods, decreasing = FALSE) # due to occasional error introduced by aggregation, 
   # on steep slopes, smaller floods may produce higher WSE elevations for a single tile due to aggregation.
   # While these errors reduce accuracy, sorting the floods allows the interpolation scheme to make a
@@ -118,12 +126,12 @@ for (i in 1:length(rt_map@data@values)) {
   if(method == 'log-linear')(vals[i] <- 1/((10^((
     (elevations[i]-max(floods[which(floods<elevations[i])]))*
       
-      (log10(max(probs[which(floods>elevations[i])]))-log10(min(probs[which(floods<elevations[i])])))/
+      (log10(max(probs_pct[which(floods>elevations[i])]))-log10(min(probs_pct[which(floods<elevations[i])])))/
       
       (min(floods[which(floods>elevations[i])])-max(floods[which(floods<elevations[i])]))
     
-  )+log10(min(probs[which(floods<elevations[i])]))))/100)) # official FEMA log-linear interpolation formula
-  else if(method == 'spline')(vals[i] <- spline(floods,returns,xout = elevations[i], method = 'hyman')$y)
+  )+log10(min(probs_pct[which(floods<elevations[i])]))))/100)) # official FEMA log-linear interpolation formula
+  else if(method == 'spline')(vals[i] <- spline(floods,return_periods,xout = elevations[i], method = 'hyman')$y)
 }
 close(pb)
 end <- Sys.time()
@@ -131,46 +139,46 @@ end-start
 
 ######################### Post process interpolated flood probability data
 # return periods cannot be negative, replace negative return period with the minimum return period in the range
-vals<-replace(vals,which(vals<min(returns)),min(returns))
+vals<-replace(vals,which(vals<min(return_periods)),min(return_periods))
 # return periods over 500 are extrapolating beyond the data, set them to NA
-vals<-replace(vals,which(vals>max(returns)),NA)
+vals<-replace(vals,which(vals>max(return_periods)),NA)
 # if the wse of the largest flood is less than the elevation, set the pixel value to NA.
-vals<-replace(vals, which(getValues(flood_raster_interp[[len]])<getValues(elev_agg)), NA)
+vals<-replace(vals, which(getValues(returnLevels_wse_smooth[[n_rp]])<getValues(dem_upscaled)), NA)
 # if the wse of the a flood is greater than the elevation, and the return period is NA, 
 # set the pixel value to the return period of that flood.
 
-for (i in 1:len) {
-  vals<-replace(vals, intersect(which(getValues(flood_raster_interp[[i]])>getValues(elev_agg)), which(is.na(vals)==TRUE)), returns[i])
+for (i in 1:n_rp) {
+  vals<-replace(vals, intersect(which(getValues(returnLevels_wse_smooth[[i]])>getValues(dem_upscaled)), which(is.na(vals)==TRUE)), return_periods[i])
 }
 
-for (i in 1:len) {
-  vals<-replace(vals, intersect(which(is.na(getValues(flood_raster_agg[[i]]))==FALSE), which(vals>returns[i])), returns[i])
+for (i in 1:n_rp) {
+  vals<-replace(vals, intersect(which(is.na(getValues(returnLevels_wse_upscaled[[i]]))==FALSE), which(vals>return_periods[i])), return_periods[i])
 }
 
-vals<-replace(vals, intersect(which(is.na(getValues(flood_raster_agg[[len]]))==FALSE), which(is.na(vals)==TRUE)), returns[len])
+vals<-replace(vals, intersect(which(is.na(getValues(returnLevels_wse_upscaled[[n_rp]]))==FALSE), which(is.na(vals)==TRUE)), return_periods[n_rp])
 
-vals<-replace(vals, which(is.na(getValues(flood_raster_agg[[len]]))==TRUE), NA)
+vals<-replace(vals, which(is.na(getValues(returnLevels_wse_upscaled[[n_rp]]))==TRUE), NA)
 # Ensure proper flooding extent: Slight mistmatches between flood elevation and the DEM may leave areas in 
 # the 500 year zone in the 100 year zone
 
-for (i in 1:(len-1)) {
-  vals<-replace(vals, intersect(intersect(which(is.na(getValues(flood_raster_agg[[i+1]]))==FALSE), 
-                                          which(is.na(getValues(flood_raster_agg[[i]]))==TRUE)),
-                                which((vals<returns[i]))), returns[i])
+for (i in 1:(n_rp-1)) {
+  vals<-replace(vals, intersect(intersect(which(is.na(getValues(returnLevels_wse_upscaled[[i+1]]))==FALSE), 
+                                          which(is.na(getValues(returnLevels_wse_upscaled[[i]]))==TRUE)),
+                                which((vals<return_periods[i]))), return_periods[i])
 }
 
 
-#which(is.na(getValues(flood_raster_agg[[i]]))==TRUE)
+#which(is.na(getValues(returnLevels_wse_upscaled[[i]]))==TRUE)
 
 ########################## recreate flood zones
 # create new raster
-floodzones <- elev_agg
+floodzones <- dem_upscaled
 # create vector where all land above 500 yr flood is NA
-bounds500 <- getValues(flood_raster_agg[[which(returns==500)]])
+bounds500 <- getValues(returnLevels_wse_upscaled[[which(return_periods==500)]])
 # replace all non NA values with 500
 bounds500 <- replace(bounds500, which(bounds500<Inf),500)
 # create vector where all land above 100 yr flood is NA
-bounds100 <- getValues(flood_raster_agg[[which(returns==100)]])
+bounds100 <- getValues(returnLevels_wse_upscaled[[which(return_periods==100)]])
 # replace all non NA values with 100
 bounds <- replace(bounds500, which(bounds100<Inf),100)
 # set raster values to be either NA, 500, 100, or 1 to define the FEMA flood zones and mean WSE
